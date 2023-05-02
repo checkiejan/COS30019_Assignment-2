720f7122ddebd19d0b887baa009c3f42c73c30a3
hung <hungtrangia2003@gmail.com>
2023-05-02 15:22:22 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+with open(sys.argv[2],"r") as f: 
+    track = f.readline().strip("[\n]")
+    track = f.readline().strip("[\n]")[:-1].split(";")
+    print(track)
+    
+
+methods = ["tt","fc","bc"]
+if len(sys.argv) == 3:
+    method = sys.argv[1].lower()
+    
+    if method in methods:
+        pass
+       
+    else:
+        print("Unknown method")
+elif len(sys.argv) > 2:
+    print("wrong number of argument")
+else:
+    pass
