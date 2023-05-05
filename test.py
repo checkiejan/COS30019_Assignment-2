515696508d0d5594acbcac431073144091745ac3
hung <hungtrangia2003@gmail.com>
2023-05-05 21:29:43 +1000

    sentence

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import re
-string = "b&e => f"
-string = string.strip(" ")
-print(re.split("[&] =>",string))
+from sentence import Sentence
+string ="f1"
+print(re.findall("[a-zA-Z0-9]+|[&]|[~]|[|]+|\B=>", string))
+sentence = Sentence(string)
+#sentence.setValue({"b1": True, "e1": True,"f": False})
+sentence.setValue({"f1": False})
+print(sentence.result())
